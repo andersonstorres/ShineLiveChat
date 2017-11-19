c26faf70d04d49974e941696f3a65bf0b543cb71
torres <andersonstorres@gmail.com>
2017-11-19 14:51:30 -0200

    first commit

---
+++ ShineLiveChat/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { LiveCamPage } from '../pages/liveCam/liveCam';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    LiveCamPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    LiveCamPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ ShineLiveChat/src/pages/about/about.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  public base64Image: string;
+
+   constructor(private camera: Camera) { }
+
+
+
+  takePicture(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    console.log('teste');
+
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  escreva(){
+
+    console.log('teste');
+    alert('start of function x');
+
+  }
+
+}
